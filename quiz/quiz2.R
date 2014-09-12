eb77af61d32601433ee4e6ae9b2d63ee543d0b91
gdwangh <gdwangh@189.cn>
2014-09-12 17:45:38 +0800

    quiz2 Q1~Q4

---
+++ quiz/quiz2.R
@@ -0,0 +1,14 @@
+# 1,2
+x <- c(0.61, 0.93, 0.83, 0.35, 0.54, 0.16, 0.91, 0.62, 0.62)
+y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
+
+fit<-lm(y~x)
+summary(fit)
+
+# 3
+library(datasets)
+data(mtcars)
+fit<-lm(mpg~wt,data=mtcars)
+summary(fit)
+
+predict(fit, newdata=data.frame(wt=mean(mtcars$wt)),interval="confidence")
